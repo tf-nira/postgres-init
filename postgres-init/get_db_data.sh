943193b444e2c49bb0d34a8106e0cb98af27a617
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2022-02-18 10:06:13 +0530

    added flow for docker build.

--- postgres-init/get_db_data.sh
+++ postgres-init/get_db_data.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p ./repos
-
 while IFS= read -r line; do
   REPO_NAME=$line
   git clone https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-done < ./postgres-init/repo-list.txt
+done < ./repo-list.txt
 
