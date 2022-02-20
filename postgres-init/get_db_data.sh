c008a167fbc0f5e63be1215aa3892fa0e1daa4de
Mandeep Dhiman <mandeep.kumar@technoforte.co.in>
2022-02-20 14:18:21 +0530

    Update get_db_data.sh

--- postgres-init/get_db_data.sh
+++ postgres-init/get_db_data.sh
@@ -2,10 +2,10 @@
 
 while IFS= read -r line; do
   REPO_NAME=$line
-  if [ $REPO_NAME == "keymanager" ]; then
-  git clone -b release-1.2.0 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  else
+  if [ $REPO_NAME == "admin-services" ]; then
   git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+  else
+  git clone -b release-1.2.0 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
   fi
 done < ./repo-list.txt
 
