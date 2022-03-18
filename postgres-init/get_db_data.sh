c5516265cc54410c2134c63e8ed75317e234c886
Puneet Joshi <puneet@mosip.io>
2022-03-18 10:49:02 +0530

    Reorg. Manual procedure added. README added.

--- postgres-init/get_db_data.sh
+++ postgres-init/get_db_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# Usage: ./get_db_data.sh <branch>
 while IFS= read -r line; do
   REPO_NAME=$line
   git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
