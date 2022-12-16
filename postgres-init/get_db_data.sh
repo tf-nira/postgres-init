ef0a653a6467675e795af6cdea6d794f4b748bfb
ckm007 <chandrakeshavmishra@gmail.com>
2022-12-16 16:18:24 +0530

    [DSD-1805] updated changes for postgres

--- postgres-init/get_db_data.sh
+++ postgres-init/get_db_data.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 while IFS= read -r line; do
-  REPO_NAME=$line
-  git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+REPO_NAME=$line
+if  (( $REPO_NAME == "resident-services" )); then
+git clone -b develop https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+elif (( $REPO_NAME == "mosip-compliance-toolkit" )); then
+git clone -b v0.0.9 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+elif (( $REPO_NAME == "digital-card-service")) ; then
+git clone -b develop https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+else (( $REPO_NAME == "idp")) ; then
+git clone -b develop https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+else
+git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+fi
 done < ./repo-list.txt
 
