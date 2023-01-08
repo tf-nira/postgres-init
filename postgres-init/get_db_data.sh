c9aeef6edb675c4948b36a14b93cbf19dff9b85c
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-01-08 15:25:28 +0530

    [MOSIP-25705] updated for branch renaming purpose

--- postgres-init/get_db_data.sh
+++ postgres-init/get_db_data.sh
@@ -15,18 +15,6 @@ elif [ $REPO_NAME == "digital-card-service" ]
 then
 branch=develop
 git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-elif [ $REPO_NAME == "idp" ]
-then
-branch=0.9.0
-git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-elif [ $REPO_NAME == "id-authentication" ]
-then
-branch=1.2.0.1-B2
-git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-elif [ $REPO_NAME == "partner-management-services" ]
-then
-branch=1.2.0.1-B2
-git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
 else
 git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
 fi
