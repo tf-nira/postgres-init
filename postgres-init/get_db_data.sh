c148ec56a7f60c72480e46cf5865c5930d6470ef
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2023-03-30 22:17:09 +0530

    [ DSD-2389 ] updated get_db_data.sh (#46)
    Co-authored-by: syed-salman-technoforte <syed.salman@technoforte.co.in>

--- postgres-init/get_db_data.sh
+++ postgres-init/get_db_data.sh
@@ -2,6 +2,9 @@
 # Usage: ./get_db_data.sh <branch>
 while IFS= read -r line; do
   REPO_NAME=$line
+  if [ -z $line ]; then
+    continue;
+  fi
   git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
 done < ./repo-list.txt
 
