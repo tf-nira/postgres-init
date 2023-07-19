a9d251965814de4fedc0bf9dbb0a451a31086d9d
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-07-19 16:19:41 +0530

    [MOSIP-28457] added compliance toolkit (#70)

--- postgres-upgrade/get_db_data.sh
+++ postgres-upgrade/get_db_data.sh
@@ -2,6 +2,15 @@
 
 while IFS= read -r line; do
   REPO_NAME=$line
+  if [ -z $line ]; then
+    continue;
+  fi
+  if [ $REPO_NAME == "mosip-compliance-toolkit" ]
+  then
+  branch=release-1.x.x
+  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
+  else
   git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-done < ./repo-list.txt
+  fi
+  done < ./repo-list.txt
 
