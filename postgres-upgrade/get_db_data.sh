7b2ed45220c98506c6154bffee8e02c4d6c42de5
Mohan E <mohanraj1715@gmail.com>
2024-03-14 11:50:07 +0530

    [MOSIP-31951] Updated get_db_data.sh (#105)

--- postgres-upgrade/get_db_data.sh
+++ postgres-upgrade/get_db_data.sh
@@ -7,51 +7,7 @@ while IFS= read -r line; do
   fi
   if [ $REPO_NAME == "mosip-compliance-toolkit" ]
   then
-  branch=v1.3.2
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "esignet" ]
-  then
-  branch=v1.3.0
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "commons" ]
-  then
-  branch=v1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "id-authentication" ]
-  then
-  branch=release-1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "keymanager" ]
-  then
-  branch=v1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "audit-manager" ]
-  then
-  branch=v1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "admin-services" ]
-  then
-  branch=v1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "id-repository" ]
-  then
-  branch=v1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "partner-management-services" ]
-  then
-  branch=v1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "pre-registration" ]
-  then
-  branch=v1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "registration" ]
-  then
-  branch=v1.2.0.1
-  git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
-  elif [ $REPO_NAME == "resident-services" ]
-  then
-  branch=release-1.2.0.1
+  branch=release-1.x.x
   git clone -b $branch https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
   else
   git clone -b $1 https://github.com/mosip/$REPO_NAME.git ./repos/$REPO_NAME
